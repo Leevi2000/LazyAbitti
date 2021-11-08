6e61caf0b109d4c8b2aae6f58adc084b6bc2ceb1
Leevi Eskola <leevieskola@gmail.com>
2021-11-08 14:59:48 +0200

    Able to compare newest version available and version installed

---
+++ Main/Main/Main.py
@@ -0,0 +1,18 @@
+import CheckUpdates
+
+if bool(CheckUpdates.CheckForUpdates()):
+    print("Update Available")
+    StartUpdate()
+
+
+def StartUpdate():
+    print("Starting update")
+    
+
+
+
+
+
+
+
+
